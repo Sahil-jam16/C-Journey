5caa0efada18d69ed6f749ed43c27ba5336e653b
Sahil-jam16 <sahiljamadar16@gmail.com>
2022-01-16 20:18:42 +0530

    Maximum And Minimum of An Array

---
+++ 0003_Max_and_Min_of_Array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+	int t,n,a[50],i,j,k,max,min;
+	scanf("%d",&t);
+	for( i=1 ; i<=t ; i++)
+    {
+        scanf("%d",&n);
+        for( j=0 ; j<n ; j++)
+        {
+            scanf("%d ",&a[j]);
+        }
+        max=min=a[0];
+            for( j=0 ; j<n ; j++ )
+            {
+                if( a[j]<min) 
+                 min=a[j];
+                if( a[j]>max )
+                  max=a[j];
+            }
+            printf("%d %d\n",max,min);
+        
+        
+    }    
+
+	return 0;
+}
+    
+    
+	
