b07660067257f54559ad8a862525a66687999a55
Sahil-jam16 <sahiljamadar16@gmail.com>
2022-01-20 21:15:13 +0530

    Optimized

--- 0009_Example_structure_problem.c
+++ 0009_Example_structure_problem.c
@@ -12,7 +12,7 @@ struct product
 main()
 {
       
-int n,temp,bill;
+int n;
 system("cls");
 printf("Enter the no of products : ");
 scanf("%d",&n);
@@ -29,14 +29,14 @@ for( int i=1 ; i<=n ; i++)
     printf("\nEnter the quantity of Product %d :",i);
     scanf("%d",&pro[i].quantity);
 }
-bill=0;
-printf("\n\t\t\tBILL SCRIPT\n\n\n");
+float bill=0;
+printf("\n\t\t\tBILL SCRIPT\n\n");
 printf("S.no\tName\t\tPrice\t\tQuantity\t\t\n\n");
 for( int j=1 ; j<=n ; j++ )
 {
-    printf("%2d\t%-15s\t\t%-15.2f\t%-2d\n\n",pro[j].pcode,pro[j].pname,pro[j].price,pro[j].quantity);
+    printf("%2d\t%-15s\t%-5.2f\t\t%-2d\n\n",pro[j].pcode,pro[j].pname,pro[j].price,pro[j].quantity);
     bill=bill+(pro[j].quantity*pro[j].price);   
 }
-printf("\n\tTOTAL AMOUNT TO BE PAID : %d",bill);
+printf("\n\tTOTAL AMOUNT TO BE PAID : %5.2f\n\n",bill);
 
 }
